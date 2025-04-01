4b8a94aee80ddd5f2e3af33418fe2079b74fbbeb
kriped <89198859+kriped@users.noreply.github.com>
2025-04-01 14:41:05 +0200

    removed redundant files

--- code/Result_splitting.m
+++
@@ -1,6 +0,0 @@
-%Code to split the RESULT.mat file into several files relevant to the
-%MScNPP code
-function Result_splitting(input_dir)
-    load(input_dir + "RESULTS.mat","FLX1","FLX2","keff")
-    save(input_dir + "FUM_data.mat","FLX1","FLX2","keff")
-end
